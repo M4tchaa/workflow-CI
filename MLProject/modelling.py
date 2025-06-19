6b0a2756ec9bf94b68537ef1bd69785d144c0147
M4tchaa <arlialya@gmail.com>
2025-06-20 01:14:55 +0700

    remove nested run

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -3,13 +3,12 @@ import mlflow
 import mlflow.sklearn
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, f1_score
-from sklearn.model_selection import train_test_split
 import joblib
 
-# Aktifkan autolog MLflow
+# Aktifkan autologging
 mlflow.autolog()
 
-# Load dataset hasil preprocessing
+# Load dataset
 train_df = pd.read_csv('train.csv')
 test_df = pd.read_csv('test.csv')
 
@@ -18,19 +17,15 @@ y_train = train_df['Sales_Class']
 X_test = test_df.drop('Sales_Class', axis=1)
 y_test = test_df['Sales_Class']
 
-# Set experiment
-mlflow.set_experiment("ps4-games-sales-classification")
+# Training
+model = LogisticRegression(max_iter=200)
+model.fit(X_train, y_train)
 
-# Jalankan training dan logging dengan MLflow
-with mlflow.start_run(nested=True):
-    model = LogisticRegression(max_iter=200)
-    model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
 
-    y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred)
+print(f"Akurasi: {acc:.4f} | F1-score: {f1:.4f}")
 
-    print(f"Akurasi: {acc:.4f} | F1-score: {f1:.4f}")
-
-    # Simpan model ke file
-    joblib.dump(model, "model.pkl")
+# Simpan model
+joblib.dump(model, "model.pkl")
